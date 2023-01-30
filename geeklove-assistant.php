622dcd87f0884a4319960fd25908b3cb79c9ec05
Krishna Kant <lushkant@gmail.com>
2023-01-30 16:18:02 +0530

    Bump version

--- geeklove-assistant.php
+++ geeklove-assistant.php
@@ -5,7 +5,7 @@
  * Description: A plugin to assist Geeklove theme in adding widgets.
  * Author: Codestag
  * Author URI: https://codestag.com
- * Version: 1.0.1
+ * Version: 1.1.0
  * Text Domain: geeklove-assistant
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
